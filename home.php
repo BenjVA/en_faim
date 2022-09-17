e14367881c88678b31f7c16482caa0ec22aa056f
BenjVA <ben.valette@live.fr>
2022-09-17 16:00:34 +0200

    Création des fichiers pour le site

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenue chez En faim !</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <?php include('header.php'); ?>
+
+        <div id="corps">
+            <h1>Réservation</h1>
+            
+
+
+        </div>
+    </body>
+</html>
